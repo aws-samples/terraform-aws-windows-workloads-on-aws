fa2e8ff2d5197e2887fb19b4bc7a33abbed92c61
Marcio Morales <momarcio@amazon.com>
2022-08-10 10:37:44 -0500

    Removing folders

--- terraform-aws-beanstalk/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "beanstalk_net_framework_uri" {
-  value = aws_elastic_beanstalk_environment.net_framework_environment.endpoint_url
-}
-
-output "beanstalk_net_framework_cname" {
-  value = aws_elastic_beanstalk_environment.net_framework_environment.cname
-}
