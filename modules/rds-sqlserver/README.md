7fe95001dd76cfe2b42bc3e6f383106e3624c21b
Juan Pablo <juanpamz@gmail.com>
2022-08-11 12:07:43 +0200

    Modified rds-sqlserver module

---
+++ modules/rds-sqlserver/README.md
@@ -0,0 +1,38 @@
+# Amazon RDS for SQL Server
+
+Terraform module which creates and RDS instance for SQL Server.
+
+## Providers
+
+- hashicorp/aws | version = "~> 4.0"
+
+## Variables description
+- **rdssql_db_subnet_group_name (string)**: Name for the DB subnet group
+- **rdssql_ingress_name (string)**: Security Group name
+- **rdssql_ingress_ports (list(number))**: List of ports opened from Private Subnets CIDR to RDS SQL Instance
+- **ManagedPolicy (list(any))**: Managed policy for making calls to your directory
+- **rdssql_engine (ist(any))**: SQL Server Version
+- **rdssql_engine_version (list)**: 15.00 = SQL Server 2019 / 14.00 = SQL Server 2017 / 13.00 = SQL Server 2016 / 12.00 = SQL Server 2014
+- **rdssql_password (string)**: RDS Admin password
+- **time_zone (string)**: Database timezone
+- **sql_collation (string)**: SQL Server Collation
+- **backup_windows_retention_maintenance (list(any))**: Backup window time, desired retention in days, maitenance windows
+- **rds_db_instance_class (string)**: Amazon RDS DB Instance class
+- **storage_allocation (list(any))**: Allocated storage Gb, Max allocated storage Gb
+- **user_name (string)**: SQL Server Admin username
+
+
+## Usage
+
+```hcl
+module "rds-sqlserver" {
+  source  = "aws-samples/windows-workloads-on-aws/aws//modules/rds-sqlserver"
+  version = "1.0.2"
+
+  rds_db_instance_class = "db.t3.medium"
+  user_name = "admin_mssql"
+}
+```
+## Outputs
+
+- **application_url**: URL to the AWS App Runner application 
--- modules/rds-sqlserver/variables.tf
+++ modules/rds-sqlserver/variables.tf
@@ -2,19 +2,20 @@
 variable "rdssql_db_subnet_group_name" {
   type    = string
   default = "rdssql_db_subnet_group"
+  description = "Name for the DB subnet group"
 }
 
 
 variable "rdssql_ingress_name" {
   type        = string
-  description = "Security Group name"
   default     = "Amazon RDS for SQL Server - Security Group"
+  description = "Security Group name"
 }
 
 variable "rdssql_ingress_ports" {
   type        = list(number)
-  description = "List of ports opened from Private Subnets CIDR to RDS SQL Instance"
   default     = [1433]
+  description = "List of ports opened from Private Subnets CIDR to RDS SQL Instance"
 }
 
 ## Iam Roles for Domain join
@@ -22,63 +23,64 @@ variable "rdssql_ingress_ports" {
 variable "ManagedPolicy" {
   type    = list(any)
   default = ["arn:aws:iam::aws:policy/service-role/AmazonRDSDirectoryServiceAccess"]
+  description = "Managed policy for making calls to your directory"
 }
 
 ## Amazon RDS for SQL Server
 
 variable "rdssql_engine" {
   type        = list(any)
-  description = "SQL Server Version"
   default     = ["sqlserver-ex", "sqlserver-web", "sqlserver-se", "sqlserver-ee"]
+  description = "SQL Server Version"
 }
 
 variable "rdssql_engine_version" {
   type        = list
-  description = "15.00 = SQL Server 2019 / 14.00 = SQL Server 2017 / 13.00 = SQL Server 2016 / 12.00 = SQL Server 2014"
   default     = ["15.00", "14.00", "13.00", "12.00"]
+  description = "15.00 = SQL Server 2019 / 14.00 = SQL Server 2017 / 13.00 = SQL Server 2016 / 12.00 = SQL Server 2014"
 }
 
 variable "rdssql_password" {
-  description = "RDS Admin password"
   type        = string
   default = "MyStrongPa$$w0rd"
+  description = "RDS Admin password"
   sensitive   = true
   ## Terraform _ Sensitive Variables = https://learn.hashicorp.com/tutorials/terraform/sensitive_variables
 }
 
 variable "time_zone" {
-  description = "Database timezone"
   type        = string
   default     = "GMT Standard Time"
+  description = "Database timezone"
 }
 
 variable "sql_collation" {
-  description = "SQL Server Collation"
   type        = string
   default     = "SQL_Latin1_General_CP1_CI_AS"
+  description = "SQL Server Collation"
 }
 
 variable "backup_windows_retention_maintenance" {
-  description = "Backup window time, desired retention in days, maitenance windows"
   type        = list(any)
   default     = ["03:00-06:00", "35", "Mon:00:00-Mon:03:00"]
+  description = "Backup window time, desired retention in days, maitenance windows"
 }
 
 variable "rds_db_instance_class" {
-  description = "Amazon RDS DB Instance class"
   type        = string
   default     = "db.t3.medium"
+  description = "Amazon RDS DB Instance class"
   # Instance type: https://aws.amazon.com/rds/sqlserver/instance_types/
 }
 
 variable "storage_allocation" {
-  description = "Allocated storage Gb, Max allocated storage Gb"
   type        = list(any)
   default     = ["20", "100"]
+  description = "Allocated storage Gb, Max allocated storage Gb"
 }
 
 variable "user_name" {
-  description = "SQL Server Admin username"
   type        = string
   default     = "admin_mssql"
+  description = "SQL Server Admin username"
 }
