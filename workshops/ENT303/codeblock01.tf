c02daf9020d524842f245837b611c6c1c46d759c
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2022-11-24 10:29:45 +0000

    Update codeblock01.tf

--- workshops/ENT303/codeblock01.tf
+++ workshops/ENT303/codeblock01.tf
@@ -1,4 +1,5 @@
 # Create a Resource Group for Terraform created instances
+
 resource "aws_resourcegroups_group" "pdo" {
   name        = format("%s%s%s%s", var.customer_code, "rgg", var.environment_code, "pdo")
   description = "Planetry Defence Organization environment resources"
