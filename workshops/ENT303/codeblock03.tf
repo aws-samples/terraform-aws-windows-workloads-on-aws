6bf7d2eca8789a2ba0f21eea5400cf2679e790db
Mo Mohoboob <momohob@amazon.com>
2023-01-08 16:29:03 +0000

    added support for additional regions

--- workshops/ENT303/codeblock03.tf
+++ workshops/ENT303/codeblock03.tf
@@ -32,12 +32,28 @@ data "aws_iam_policy_document" "websrv" {
   statement {
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::652711504416:root",
-                     "arn:aws:iam::156460612806:root",
-                     "arn:aws:iam::127311923021:root",
-                     "arn:aws:iam::033677994240:root",
-                     "arn:aws:iam::027434742980:root",
-                     "arn:aws:iam::797873946194:root"]
+      identifiers = ["arn:aws:iam::127311923021:root", #US East (N. Virginia) 
+                     "arn:aws:iam::033677994240:root", #US East (Ohio)
+                     "arn:aws:iam::027434742980:root", #US West (N. California)
+                     "arn:aws:iam::797873946194:root", #US West (Oregon)
+                     "arn:aws:iam::098369216593:root", #Africa (Cape Town)
+                     "arn:aws:iam::754344448648:root", #Asia Pacific (Hong Kong)
+                     "arn:aws:iam::589379963580:root", #Asia Pacific (Jakarta)
+                     "arn:aws:iam::718504428378:root", #Asia Pacific (Mumbai)
+                     "arn:aws:iam::383597477331:root", #Asia Pacific (Osaka)
+                     "arn:aws:iam::600734575887:root", #Asia Pacific (Seoul)
+                     "arn:aws:iam::114774131450:root", #Asia Pacific (Singapore)
+                     "arn:aws:iam::783225319266:root", #Asia Pacific (Sydney)
+                     "arn:aws:iam::582318560864:root", #Asia Pacific (Tokyo) 
+                     "arn:aws:iam::985666609251:root", #Canada (Central) 
+                     "arn:aws:iam::054676820928:root", #Europe (Frankfurt)
+                     "arn:aws:iam::156460612806:root", #Europe (Ireland)
+                     "arn:aws:iam::652711504416:root", #Europe (London)
+                     "arn:aws:iam::635631232127:root", #Europe (Milan)
+                     "arn:aws:iam::009996457667:root", #Europe (Paris) 
+                     "arn:aws:iam::897822967062:root", #Europe (Stockholm)
+                     "arn:aws:iam::076674570225:root", #Middle East (Bahrain)
+                     "arn:aws:iam::507241528517:root"] #South America (São Paulo) 
     }
 
     actions = [
